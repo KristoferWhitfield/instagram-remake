47d0ea288929eaee5113e202f57188ef0220ecab
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-11-06 19:35:02 -0400

    instagram

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+
+const dbName = "instagram";
+
+module.exports = {
+  // url: "mongodb+srv://UrjiH:getpassword@cluster0.rh8ci.mongodb.net/insta?retryWrites=true&w=majority",
+  url: "mongodb+srv://KW1234:test1234@cluster0.jcysh.mongodb.net/instagram?retryWrites=true&w=majority",
+};
